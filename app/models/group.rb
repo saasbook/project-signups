21ca3e4b22778dcfd8c10d22a61b1f19aa5e9047
Richard Xia <rxia@eecs.berkeley.edu>
2012-02-13 23:07:48 -0800

    Added various niceties to views

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,7 @@
 class Group < ActiveRecord::Base
+  has_many :students
+
+  def student_names
+    students.map{|x| x.full_name}
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ActiveRecord::Base
+  belongs_to :group
+
+  def full_name
+    first = nick_name.empty? ? first_name : nick_name
+    "#{first} #{last_name}"
+  end
 end
