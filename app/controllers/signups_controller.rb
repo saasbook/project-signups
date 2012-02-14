180448991bff533df3ec27901a8b76e235ce8ecb
Richard Xia <rxia@eecs.berkeley.edu>
2012-02-13 23:29:09 -0800

    Added error messages for signup page

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -3,4 +3,25 @@ class SignupsController < ApplicationController
     @groups = Group.all
     @projects = Project.all
   end
+
+  def create
+    group_id = params[:group_id]
+    first_choice = params[:first_choice]
+    second_choice = params[:second_choice]
+    third_choice = params[:third_choice]
+
+    begin
+      ProjectPreference.transaction do
+        ProjectPreference.create!(:group_id => group_id, :level => 1, :project_id => first_choice)
+        ProjectPreference.create!(:group_id => group_id, :level => 2, :project_id => second_choice)
+        ProjectPreference.create!(:group_id => group_id, :level => 3, :project_id => third_choice)
+      end
+
+      flash[:notice] = "Successfully submitted project preferences"
+      redirect_to action: 'new'
+    rescue Exception => e
+      flash[:error] = "Error saving preferences: #{e}"
+      redirect_to action: 'new'
+    end
+  end
 end
--- app/models/project_preference.rb
+++ app/models/project_preference.rb
@@ -1,2 +1,9 @@
 class ProjectPreference < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :project
+
+  validates :level, 
+    :uniqueness => { :scope => :group_id, :message => "Only one preference per preference level per group" }, 
+    :inclusion => { :in => [1, 2, 3] }
+  validates :project_id, :uniqueness => { :scope => :group_id, :message => "Each preference level must have a different project" }
 end
