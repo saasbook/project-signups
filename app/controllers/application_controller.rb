71e9dab3ff143a0836eb8a765e40962ab70db2d0
ken yu <kenniyu@gmail.com>
2012-09-10 19:29:17 -0700

    backwards compatibility for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_authorization
+
+  protected
+
+  def check_authorization
+    @authorized ||= admin_signed_in?
+  end
+
+  def authorize!
+    if !admin_signed_in?
+      flash[:error] = "You do not have permission to access this page"
+      redirect_to :root and return false
+    end
+  end
 end
