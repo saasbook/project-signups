16178c23ac69fbdfcd3d795e316eb6ab79746ce9
ken yu <kenniyu@gmail.com>
2012-09-10 19:25:15 -0700

    took out unnecessary code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :check_authorization
-
-  protected
-
-  def check_authorization
-    @authorized ||= session[:passwd] == "password"
-  end
-
-  def authorize!
-    if session[:passwd] != "password"
-      flash[:error] = "You do not have permission to access this page"
-      redirect_to :root and return false
-    end
-  end
 end
