30f2debaffc37b36b5e67558c4bb08f930b12fb3
ken yu <kenniyu@gmail.com>
2012-09-10 22:02:44 -0700

    modified seed file to create admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create({ :email => 'admin@cs169.berkeley.edu', :password => 'password', :password_confirmation => 'password'})
